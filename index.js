622b0c04d236f3812197e0827acb4425596ef8ca
oliver butterley <oliver.butterley@gmail.com>
2023-06-06 14:09:01 +0200

    Basic but complete

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
 const core = require('@actions/core');
-const wait = require('./wait');
+const github = require('@actions/github');
+const pdflib = require('pdf-lib');
+const fs = require('fs');
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+    try {
+        const inputFilename = core.getInput('filename');
+        const text = core.getInput('text')
+        const shaText = `sha: ${github.context.sha}`
+        const uint8Array = fs.readFileSync(inputFilename)
+        const pdfDoc = await pdflib.PDFDocument.load(uint8Array)
+        const courierFont = await pdfDoc.embedFont(pdflib.StandardFonts.Courier)
+        console.log(`filename: ${inputFilename}, text: ${text}, sha: ${shaText}`)
+        const pages = pdfDoc.getPages()
+        const firstPage = pages[0]
+        const textSize = 12;
+        const textWidth = courierFont.widthOfTextAtSize(text, textSize);
+        firstPage.drawText(text, {
+            x: firstPage.getWidth() / 2 - textWidth / 2,
+            y: 32,
+            size: textSize,
+            font: courierFont,
+            color: pdflib.rgb(0.4, 0.4, 0.4),
+        })        
+        const shaTextWidth = courierFont.widthOfTextAtSize(shaText, textSize);
+        firstPage.drawText(shaText, {
+            x: firstPage.getWidth() / 2 - shaTextWidth / 2,
+            y: 18,
+            size: textSize,
+            font: courierFont,
+            color: pdflib.rgb(0.4, 0.4, 0.4),
+        })    
+        const pdfBytes = await pdfDoc.save()
+        fs.writeFileSync(inputFilename, pdfBytes)
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
-run();
+run()
